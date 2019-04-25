e0d4afe7d86a47e4f4c595f8fd48cedb81378395
V Kapoor <vkapoor@spartaglobal.com>
2019-04-25 10:45:18 +0100

    Added the XMLFileReader class

---
+++ src/main/java/com/sparta/vk/XMLFileReader.java
@@ -0,0 +1,30 @@
+package com.sparta.vk;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+public class XMLFileReader {
+
+    private Document parsedXMLFile;
+
+    public XMLFileReader(String xmlFilePath) {
+        try {
+        File xmlFile = new File(xmlFilePath);
+        DocumentBuilderFactory xmlFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder xmlBuilder = xmlFactory.newDocumentBuilder();
+        parsedXMLFile = xmlBuilder.parse(xmlFile);
+        } catch (IOException | ParserConfigurationException | SAXException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Document getParsedXMLFile() {
+        return parsedXMLFile;
+    }
+}
